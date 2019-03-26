689d11ba826b3397477c35606a0c6113591e2307
adenisovgit <adenisov@gmail.com>
2019-03-26 18:58:35 +0100

    добавлен полный путь к файлу теста для тестировани

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -4,7 +4,7 @@ import genDiff from '../src';
 
 test('flat JSON test', () => {
   const fileName1 = '__tests__/__fixtures__/before.json';
-  const fileName2 = '__tests__/__fixtures__/after.json';
+  const fileName2 = '/Users/bilbo/Documents/FrondEnd/hexlet/Project2/__tests__/__fixtures__/after.json';
   const result = String(readFileSync('__tests__/__fixtures__/testString1'));
   expect(genDiff(fileName1, fileName2)).toBe(result);
 });
