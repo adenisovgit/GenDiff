f571896513bd6358ae55c58d496e5c9991b5f82a
adenisovgit <adenisov@gmail.com>
2019-04-01 17:07:09 +0200

    Last (?) refactoring

--- src/renderers/diffRenderer.js
+++ src/renderers/diffRenderer.js
@@ -1,36 +1,30 @@
-import _ from 'lodash';
-
 const indentValue = (n = 1) => ` ${'    '.repeat(n)}`;
 
 const stringify = (obj, key, state, indent, level) => {
-  if (obj instanceof Object) {
-    const subLines = Object.keys(obj)
-      .map((keyMap) => {
-        const indentMap = indentValue(level + 1);
-        if (obj[keyMap] instanceof Object) {
-          return [`${indentMap}   ${keyMap}: {`, stringify(obj[keyMap], keyMap, '   ', indentMap, level + 1), `${indentMap}   }`];
-        }
-        return [`${indentMap}   ${keyMap}: ${obj[keyMap]}`];
-      });
-    return [`${indent}${state}${key}: {`, ...subLines, `${indent}   }`];
+  if (!(obj instanceof Object)) {
+    return `${indent}${state}${key}: ${obj}\n`;
   }
-  return [`${indent}${state}${key}: ${obj}`];
+
+  const subLines = Object.keys(obj)
+    .map((keyMap) => {
+      const indentMap = indentValue(level + 1);
+      return stringify(obj[keyMap], keyMap, '   ', indentMap, level + 1);
+    });
+  return `${indent}${state}${key}: {\n${subLines}${indent}   }\n`;
 };
 
 const renderDiffActions = {
-  group: (obj, indent, level, func) => [`${indent}   ${obj.key}: {`, func(obj.children, level + 1), `${indent}   }`],
-  same: (obj, indent) => [`${indent}   ${obj.key}: ${obj.value}`],
+  group: (obj, indent, level, func) => `${indent}   ${obj.key}: ${func(obj.children, level + 1)}\n`,
+  same: (obj, indent) => `${indent}   ${obj.key}: ${obj.value}\n`,
   added: (obj, indent, level) => stringify(obj.value, obj.key, ' + ', indent, level),
   deleted: (obj, indent, level) => stringify(obj.value, obj.key, ' - ', indent, level),
-  changed: (obj, indent, level) => [...stringify(obj.newValue, obj.key, ' + ', indent, level), ...stringify(obj.oldValue, obj.key, ' - ', indent, level)],
+  changed: (obj, indent, level) => `${stringify(obj.newValue, obj.key, ' + ', indent, level)}${stringify(obj.oldValue, obj.key, ' - ', indent, level)}`,
 };
 
-const renderDiffTree = (ast, level = 0) => ast.map(obj => renderDiffActions[obj
-  .type](obj, indentValue(level), level, renderDiffTree));
-
 const renderDiff = (ast, level = 0) => {
-  const result = _.flattenDeep(renderDiffTree(ast, level));
-  return ['{', ...result, '}'].join('\n');
+  const result = ast.map(obj => renderDiffActions[obj
+    .type](obj, indentValue(level), level, renderDiff));
+  return `{\n${result.join('')}${indentValue(level).slice(1)}}`;
 };
 
 export default renderDiff;
